fce2b604237bf7c92c02dc969129836ec26e4861
Unknown <jason945119@gmail.com>
2018-10-22 22:48:09 +0800

    3-1022

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,55 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/3-1022', function () {
+    $var = [1,2,3];
+
+    if($var[1]) //false 0 '0'
+    {
+        echo 'if內';
+    }
+
+    echo 'if外';
+
+    while($var[1])
+    {
+        echo '有跑到while';
+        break;
+    }
+
+    do
+    {
+        echo '有跑到do while';
+        break;
+    }while($var[1]);
+
+    switch($var[2])
+    {
+        case 1:
+            echo '我是1';
+            break;
+        case 2:
+            echo '我是2';
+            break;
+        default:
+            echo '我什麼都不是';
+    }
+
+    $userData = [
+        'name'      => 'york',
+        'birthday'  => '12/23',
+        'sex'       => 'boy'
+    ];
+
+    foreach( $userData as $row)
+    {
+        var_dump($row);
+    }
+    echo '<br>';
+    foreach( $userData as $key => $value)
+    {
+        echo "我的${key}是${value}";
+        echo '<br>';
+    }
+});
